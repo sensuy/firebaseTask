9073ad197f8db12eb4b3d4ef94dea9c18e4685fa
sensui <sensuijunior@hotmail.com>
2020-01-25 01:24:21 -0300

    First Commit

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { RouteReuseStrategy } from '@angular/router';
+
+
+
+@NgModule({
+  imports: [
+    IonicModule.forRoot()
+  ],
+  exports: [
+    BrowserModule,
+    IonicModule
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+})
+export class CoreModule { }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
-
 import { HomePage } from './home.page';
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
+ SharedModule,
     RouterModule.forChild([
       {
         path: '',
