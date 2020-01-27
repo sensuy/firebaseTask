4927218285601522c918628bcd417865ff446d49
sensui <sensuijunior@hotmail.com>
2020-01-27 01:55:42 -0300

    login com facebook

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,12 +4,16 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from 'src/environments/environment';
 
 
 @NgModule({
   imports: [
-    IonicModule.forRoot()
+    IonicModule.forRoot(),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule
   ],
   exports: [
     BrowserModule,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyC1qCFY7kT_QxWkIjAwysaMx-xXT_Jfe94',
+    authDomain: 'ionicprojects-6c1a1.firebaseapp.com',
+    databaseURL: 'https://ionicprojects-6c1a1.firebaseio.com',
+    projectId: 'ionicprojects-6c1a1',
+    storageBucket: 'ionicprojects-6c1a1.appspot.com',
+    messagingSenderId: '713674519998',
+    appId: '1:713674519998:web:2f6b7b183bd02e77c435ef'
+  }
 };
 
 /*
