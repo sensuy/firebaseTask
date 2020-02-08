a500d3f133ede78b3185f36c30a8cea8ce36df6f
sensui <sensuijunior@hotmail.com>
2020-02-08 01:27:49 -0300

    ion grid

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,13 +3,23 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
+import { LogoutButtonComponent } from './components/logout-button/logout-button.component';
+import { MenuToggleComponent } from './components/menu-toggle/menu-toggle.component';
+
 
 
 @NgModule({
+  declarations: [
+    LogoutButtonComponent,
+    MenuToggleComponent
+  ],
+  imports: [IonicModule],
   exports: [
     CommonModule,
     ReactiveFormsModule,
-    IonicModule
+    IonicModule,
+    LogoutButtonComponent,
+    MenuToggleComponent
   ]
 })
 export class SharedModule { }
